338c1db1c886cc6d129afa69399269accc2034a0
Jeffrey Lor <jlor993@gmail.com>
2021-10-11 11:54:41 -0500

    Generated and completed Articles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { UserViewComponent } from './user-view/user-view.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { FormsModule } from '@angular/forms';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ArticleViewComponent } from './article-view/article-view.component';
+import { ArticleCreateComponent } from './article-create/article-create.component';
+import { ArticleEditComponent } from './article-edit/article-edit.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { UserEditComponent } from './user-edit/user-edit.component';
     UsersComponent,
     UserViewComponent,
     UserCreateComponent,
-    UserEditComponent
+    UserEditComponent,
+    ArticlesComponent,
+    ArticleViewComponent,
+    ArticleCreateComponent,
+    ArticleEditComponent
   ],
   imports: [
     BrowserModule,
